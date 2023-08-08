6d85e95146367ed27092be123d57d511bd94946d
Thisara Kaushan <125348115+thisarakaushan@users.noreply.github.com>
2023-08-08 20:59:43 +0530

    Add files via upload

--- src/main/java/com/manager/ActorManager.java
+++ src/main/java/com/manager/ActorManager.java
@@ -1,1 +1,15 @@
+package com.highradius.manager;
 
+import java.util.List;
+import com.highradius.pojo.Actor;
+import com.highradius.exceptions.CustomException;
+
+public interface ActorManager {
+    List<Actor> getAllActors() throws CustomException;
+
+    int addActor(Actor actor) throws CustomException;
+
+    int updateActor(Actor actor) throws CustomException;
+
+    int deleteActor(Actor actor) throws CustomException;
+}
