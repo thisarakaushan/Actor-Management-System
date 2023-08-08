0089a3b19f9e523c4d273b7c06a570c7abcff53b
Thisara Kaushan <125348115+thisarakaushan@users.noreply.github.com>
2023-08-08 20:58:22 +0530

    Add files via upload

--- src/main/java/com/dao/ActorDao.java
+++ src/main/java/com/dao/ActorDao.java
@@ -1,1 +1,17 @@
+package com.highradius.Dao;
 
+import java.util.List;
+import com.highradius.pojo.Actor;
+import com.highradius.exceptions.CustomException;
+
+public interface ActorDao {
+    List<Actor> getAllActors() throws CustomException;
+
+    int addActor(Actor actor) throws CustomException;
+
+    int updateActor(Actor actor) throws CustomException;
+
+    int deleteActor(Actor actor) throws CustomException;
+
+    boolean exist(Actor actor) throws CustomException;
+}
