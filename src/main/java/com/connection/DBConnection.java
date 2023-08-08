7da9a70b9733831c9c50fe78ab3ab918ebe810fc
Thisara Kaushan <125348115+thisarakaushan@users.noreply.github.com>
2023-08-08 20:56:53 +0530

    Add files via upload

--- src/main/java/com/connection/DBConnection.java
+++ src/main/java/com/connection/DBConnection.java
@@ -1,1 +1,27 @@
+package com.highradius.connection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ * Database connection class
+ * @author thisara.kaushan
+ *
+ */
+public class DBConnection {
+    static Connection connection = null;
+    static String url = "jdbc:mysql://localhost:3306/sakila";
+    static String username = "root";
+    static String password = "root";
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(url, username, password);
+            System.out.println("Connection was successful!");
+        } catch (Exception e) {
+            System.out.println("Failed to establish the database connection: " + e.getMessage());
+        }
+        return connection;
+    }
+}
